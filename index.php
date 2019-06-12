529812bd9140b293714f2e706f663b69d4186fae
Vinícius Tessmann <vinicius@melhorenvio.com.br>
2019-06-12 20:47:34 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use SdkMelhorEnvio\Quotation;
+
+echo \SdkMelhorEnvio\Quotation::make();
---
+++ src/Quotation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SdkMelhorEnvio;
+
+Class Quotation {
+
+    public static function make()
+    {
+        return 'cotando no melhor envio';
+    }
+}
